f2246a862125fd588ebee4877002da8671fa1dc1
ruzal <rusad21@gmail.com>
2021-09-13 15:44:53 +0300

    Initial Commit

---
+++ создание ПР с переходами/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  создание ПР с переходами
+//
+//  Created by Ruzal  on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
